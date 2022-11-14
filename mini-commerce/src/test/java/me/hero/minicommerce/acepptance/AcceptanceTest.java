eeb843ae2a7b06eb95656d5764d28e47dd3a502e
Hero <icraft2170@gmail.com>
2022-11-14 23:55:23 +0900

    test : #1 상품 생성 인수테스트 CleanDataBase 제거 - Inmemory Database(H2) 를 사용함에 따라 필요없어져 제거 - 인수테스트 특성상 실제 사용 데이터베이스를 사용하는 것이 맞습니다. 때문에 추후 변경이 필요합니다.

--- mini-commerce/src/test/java/me/hero/minicommerce/acepptance/AcceptanceTest.java
+++ mini-commerce/src/test/java/me/hero/minicommerce/acepptance/AcceptanceTest.java
@@ -1,9 +1,7 @@
 package me.hero.minicommerce.acepptance;
 
 import io.restassured.RestAssured;
-import me.hero.minicommerce.utils.DatabaseCleanup;
 import org.junit.jupiter.api.BeforeEach;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.context.ActiveProfiles;
@@ -14,12 +12,8 @@ public class AcceptanceTest {
   @LocalServerPort
   int port;
 
-  @Autowired
-  private DatabaseCleanup databaseCleanup;
-
   @BeforeEach
   public void setUp() {
     RestAssured.port = port;
-    databaseCleanup.execute();
   }
 }
