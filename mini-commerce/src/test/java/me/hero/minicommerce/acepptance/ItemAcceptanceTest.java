f958303b8594cabc2fc7495156cf369731fd6f59
Hero <icraft2170@gmail.com>
2022-11-14 21:04:17 +0900

    feature : #1 인수 테스트 시나리오 작성

--- mini-commerce/src/test/java/me/hero/minicommerce/acepptance/ItemAcceptanceTest.java
+++ mini-commerce/src/test/java/me/hero/minicommerce/acepptance/ItemAcceptanceTest.java
@@ -1,8 +1,70 @@
 package me.hero.minicommerce.acepptance;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 @DisplayName("상품 관리 기능")
 public class ItemAcceptanceTest extends AcceptanceTest {
+
+
+  /**
+   * Feature : 상품 생성 기능
+   *  When : 상품 생성 요청을 한다.
+   *  Then : 상품이 생성 된다.
+   */
+  @Test
+  @DisplayName("상품 수정 기능")
+  void createItem() {
+
+  }
+
+  /**
+   * Feature : 상품 생성 기능
+   *  Given : 상품이 생성되어 있다.
+   *  When : 상품 수정 요청을 한다.
+   *  Then : 상품이 수정 된다.
+   */
+  @Test
+  @DisplayName("상품 수정 기능")
+  void modifyItem() {
+
+  }
+
+  /**
+   * Feature : 상품 삭제 기능
+   *  Given : 상품이 생성되어 있다.
+   *  When : 상품 삭제 요청을 한다.
+   *  Then : 상품이 삭제 된다.
+   */
+  @Test
+  @DisplayName("상품 삭제 기능")
+  void deleteItem() {
+
+  }
+
+  /**
+   * Feature : 상품 삭제 기능
+   *  Given : 상품이 생성되어 있다.
+   *  When : 생성된 상품 조회 요청을 한다.
+   *  Then : 상품이 조회 된다.
+   */
+  @Test
+  @DisplayName("상품 단건 조회 기능")
+  void getOneItem() {
+
+  }
+
+
+  /**
+   * Feature : 상품 삭제 기능
+   *  Given : 상품 N개가 생성되어 있다.
+   *  When : 생성된 상품 들을 조회 요청을 한다.
+   *  Then : 상품들이 조회 된다.
+   */
+  @Test
+  @DisplayName("전체 상품 조회 기능")
+  void getAllItems() {
+
+  }
 
 }
