f701fb90dfeeacafab4f9858f98233e3a6ad2761
Hero <icraft2170@gmail.com>
2022-11-15 20:07:28 +0900

    Test : #1 Item.modify() Unit Test

---
+++ mini-commerce/src/test/java/me/hero/minicommerce/item/domain/ItemTest.java
@@ -0,0 +1,24 @@
+package me.hero.minicommerce.item.domain;
+
+import static org.assertj.core.api.Assertions.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class ItemTest {
+
+
+  @Test
+  @DisplayName("상품 수정 modify() 메소드 단위테스트")
+  void modifyItem() {
+    //given
+    Item newItem = new Item("닭도리탕", 18000L);
+    ItemModifyDto params = new ItemModifyDto("닭도리탕", 20000L);
+
+    //when
+    newItem.modify(params);
+
+    //then
+    assertThat(newItem.getName()).isEqualTo(params.getName());
+    assertThat(newItem.getPrice()).isEqualTo(params.getPrice());
+  }
+}
