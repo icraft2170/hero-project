cb9a6714af5ff0e45eca3cf86ef59202bd03f051
Hero <icraft2170@gmail.com>
2023-01-03 14:14:41 +0900

    refactor - #13 CreateItemService 코틀린으로 리팩터링

---
+++ mini-commerce/src/main/kotlin/me/hero/minicommerce/item/application/CreateItemService.kt
@@ -0,0 +1,17 @@
+package me.hero.minicommerce.item.application
+
+import me.hero.minicommerce.item.application.port.`in`.CreateItemUseCase
+import me.hero.minicommerce.item.application.port.`in`.dto.CreateItemDto
+import me.hero.minicommerce.item.application.port.out.CreateItemPort
+import org.springframework.stereotype.Service
+
+@Service
+class CreateItemService(
+    val createItemPort: CreateItemPort
+) : CreateItemUseCase {
+
+    override fun createItem(itemDto: CreateItemDto): CreateItemDto {
+        val savedItem = createItemPort.saveItem(itemDto.toEntity())
+        return CreateItemDto(savedItem)
+    }
+}
